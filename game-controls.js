fd11fa142e66c55863cd93755c9b1d5332fa3396
David Fares <davidsfares@gmail.com>
2016-09-10 00:18:24 -0500

    game-controls to run program

---
+++ game-controls.js
@@ -0,0 +1,11 @@
+var script = document.createElement('script');
+script.src = 'https://rawgit.com/DavidSFares/character-game/master/character-game.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+// Shorthand for $( document ).ready()
+$(function() {
+    console.log( script.src );
+
+    prepareCanvas(document.getElementById("canvasDiv"), 490, 220);
+});
